3c805e7cd87d6215bb044ef23e8d3a3f901ad622
Kanade Yoisaki <kanade@yoisaki.com>
2026-05-09 02:35:39 +0900

    feat: one-click scene reload

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Kuchinashi.SceneFlow;
 using QFramework;
+using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.R))
+        {
+            return;
+        }
+
+        var flow = SceneFlowController.Instance;
+        if (flow == null || !flow.IsConfigured || flow.IsTransitioning)
+        {
+            return;
+        }
+
+        flow.TryRequestReloadCurrentContent();
     }
 }
